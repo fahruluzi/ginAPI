cc145083371cb71487ad2195b3189357089b15ad
Fahrul Fauzi <fahrulf594@gmail.com>
2019-08-14 11:34:12 +0700

    add auth with github + google

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Fahrul-uzi/gin-full/config"
 	"github.com/Fahrul-uzi/gin-full/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
 )
 
 func main() {
@@ -11,11 +15,17 @@ func main() {
 	config.InitDB()
 	defer config.DB.Close()
 
+	gotenv.Load()
+
 	// Router
 	router := gin.Default()
 
+	fmt.Println(os.Getenv("CLIENT_ID_GITHUB"))
+
 	v1 := router.Group("/api/v1")
 	{
+		v1.GET("/auth/:provider", routes.RedirectHandler)
+		v1.GET("/auth/:provider/callback", routes.CallbackHandler)
 		articles := v1.Group("/article")
 		{
 			articles.GET("/", routes.GetHome)

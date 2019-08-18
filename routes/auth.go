4654826b8d694a807474dee0e73623ff94b89a9f
Fahrul Fauzi <fahrulf594@gmail.com>
2019-08-18 15:12:58 +0700

    add function for create JWT-GO

--- routes/auth.go
+++ routes/auth.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/Fahrul-uzi/gin-full/models"
 
 	"github.com/Fahrul-uzi/gin-full/config"
+	"github.com/danilopolani/gocialite/structs"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
+
+// String bebas yang tau hanya server
+var JWT_SECRET = "SUPER_SECRET"
 
 // Redirect to correct oAuth URL
 func RedirectHandler(c *gin.Context) {
@@ -69,11 +77,52 @@ func CallbackHandler(c *gin.Context) {
 		return
 	}
 
-	// Print in terminal user information
-	fmt.Printf("%#v", token)
-	fmt.Printf("%#v", user)
-	fmt.Printf("%#v", provider)
+	var newUser = getOrRegisterUser(provider, user)
+	var newToken = createToken(&newUser)
 
-	// If no errors, show provider name
-	c.Writer.Write([]byte("Hi, " + user.FullName))
+	c.JSON(200, gin.H{
+		"ha":      token,
+		"data":    newUser,
+		"token":   newToken,
+		"message": "berhasil menampilkan data",
+	})
+}
+
+func getOrRegisterUser(provider string, user *structs.User) models.User {
+	var userData models.User
+
+	config.DB.Where("provider = ? AND social_id = ?", provider, user.ID).First(&userData)
+
+	if userData.ID == 0 {
+		newUser := models.User{
+			Fullname: user.FullName,
+			Email:    user.Email,
+			SocialId: user.ID,
+			Provider: provider,
+			Avatar:   user.Avatar,
+		}
+		config.DB.Create(&newUser)
+		return newUser
+	} else {
+		return userData
+	}
+}
+
+// Setting JWT
+func createToken(user *models.User) string {
+	// apa apa aja yang mau dikeluarkan
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id":   user.ID,
+		"user_role": user.Role,
+		"exp":       time.Now().AddDate(0, 0, 7).Unix(),
+		"iat":       time.Now().Unix(),
+	})
+
+	// uncode JWT
+	tokenString, err := token.SignedString([]byte(JWT_SECRET))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return tokenString
 }
